6c1228e1011765ef74161399bf1bdd3e08bef248
Javier Cordova <jcordova96@gmail.com>
2014-10-19 23:42:14 -0700

    some stuff

--- common/config/main.php
+++ common/config/main.php
@@ -3,6 +3,10 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'xtribe',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
